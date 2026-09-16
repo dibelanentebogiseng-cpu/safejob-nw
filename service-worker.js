f83964815526ccb208d79735ce99ffd583d110fb
dibelanentebogiseng-cpu <dibelanentebogiseng@gmail.com>
2026-09-16 02:50:26 -1000

    Refactor service worker for cache management
    Updated service worker to use a new cache version and added manifest.json to cached files.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,23 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('safejob-v1').then(c=>c.addAll(['./','./index.html','./style.css','./app.js'])));
+const CACHE_NAME = 'safejob-v2-white';
+const FILES = ['./','./index.html','./style.css','./app.js','./manifest.json'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => {
+      if(k !== CACHE_NAME) return caches.delete(k);
+    }))).then(()=> self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      return caches.open(CACHE_NAME).then(c => { c.put(e.request, res.clone()); return res; });
+    }).catch(()=> caches.match(e.request))
+  );
 });
