a2307a085e223975a3cd0c750a1919964b26f78d
dibelanentebogiseng-cpu <dibelanentebogiseng@gmail.com>
2026-09-16 01:36:35 -1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = 'safejob-nw-v2';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
-];
+const CACHE_NAME = "safejob-nw-v2-protected-2026";
+const urlsToCache = ["/safejob-nw/", "/safejob-nw/index.html", "/safejob-nw/style.css", "/safejob-nw/app.js", "/safejob-nw/manifest.json"];
+
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))));
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(r => r || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
 });
