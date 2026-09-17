f1a55510123723ddb9aa02761c601324c23c3cc7
dibelanentebogiseng-cpu <dibelanentebogiseng@gmail.com>
2026-09-16 22:51:38 -1000

    Refactor service worker for caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,13 @@
-const CACHE_NAME = 'safejob-v2-white';
-const FILES = ['./','./index.html','./style.css','./app.js','./manifest.json'];
-
+const CACHE_NAME = 'safejob-nw-v2';
+const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.json'];
 self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => {
-      if(k !== CACHE_NAME) return caches.delete(k);
-    }))).then(()=> self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(res => {
-      return caches.open(CACHE_NAME).then(c => { c.put(e.request, res.clone()); return res; });
-    }).catch(()=> caches.match(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(()=>caches.match('./index.html'))));
 });
